e780e7f3ca7739a1bcda5fdc54dde8383d3c29a6
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-01-06 09:28:46 +0900

    [class] 오버로딩

--- MethodOverloadTest.java
+++ MethodOverloadTest.java
@@ -4,7 +4,7 @@ public class MethodOverloadTest {
 	//Method Overloading은 동일한 이름을 갖는 메소드를 의미한다.
 	//매개변수의 개수는 갖고 자료형이 다른 오버로딩
 	
-	public int clac(int n1, int n2) {
+	public int calc(int n1, int n2) {
 		return n1 + n2;
 	}
 	
