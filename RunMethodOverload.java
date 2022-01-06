16b74ba9c74c643505b8587d148245a008c81d27
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-01-06 09:35:35 +0900

    [class] 오버로딩 실행

--- RunMethodOverload.java
+++ RunMethodOverload.java
@@ -1,13 +1,26 @@
 package test.overload;
 
+import java.util.Scanner;
+
 public class RunMethodOverload {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		MethodOverloadTest ot = new MethodOverloadTest();
-		int n1 = 5, n2 = 7;
+		
+		System.out.println("===== 두 수의 덧셈 =====");
+		System.out.print("정수 1 입력 : ");
+		int n1 = sc.nextInt();
+		System.out.print("정수 2 입력 : ");
+		int n2 = sc.nextInt();
 		System.out.printf("%d + %d = %d\n", n1, n2, ot.calc(n1, n2));
 		
-		double n3 = 12.5, n4 = 21.3;
+		System.out.println("===== 두 수의 곱셈 =====");
+		System.out.print("실수 1 입력 : ");
+		double n3 = sc.nextDouble();
+		System.out.print("실수 2 입력 : ");
+		double n4 = sc.nextDouble();
 		System.out.printf("%.1f * %.1f = %.3f\n", n3, n4, ot.calc(n3, n4));
 	}
 
