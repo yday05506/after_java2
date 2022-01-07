61a048ea4008b05afe55bf3125b8f4b2f4dbf282
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-01-07 11:34:22 +0900

    [class] TV

---
+++ day5/src/test/access/this3/RunTV.java
@@ -0,0 +1,9 @@
+package test.this3;
+
+public class RunTV {
+
+	public static void main(String[] args) {
+		TV tv1 = new TV(55, "Black", "LG전자");
+	}
+
+}
---
+++ day5/src/test/access/this3/TV.java
@@ -0,0 +1,33 @@
+package test.this3;
+
+public class TV {
+	private String producer, color;
+	private int size;
+	
+	public String getProducer() {
+		return producer;
+	}
+	public String getColor() {
+		return color;
+	}
+	public int getSize() {
+		return size;
+	}
+	
+	public TV(int size) {
+		this.size = size;
+		System.out.printf("TV( %d ) 호출 됨.\n", size);
+	}
+	
+	public TV(int size, String color) {
+		this(size);
+		this.color = color;
+		System.out.printf("TV( %d, %s ) 호출 됨.\n", size, color);
+	}
+	
+	public TV(int size, String color, String producer) {
+		this(size, color);
+		this.producer = producer;
+		System.out.printf("TV( %d, %s, %s ) 호출 됨.\n", size, color, producer);
+	}
+}
